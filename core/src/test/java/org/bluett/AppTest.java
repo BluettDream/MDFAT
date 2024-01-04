42f14be086293d96e4fabdc1ed3f86e619103515
MengHao Tian <bluetian1st@gmail.com>
2024-01-04 11:48:22 +0800

    init project

---
+++ core/src/test/java/org/bluett/AppTest.java
@@ -0,0 +1,21 @@
+package org.bluett;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public AppTest( String testName )
+    {
+        super();
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
