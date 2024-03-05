d44f8902abdbe33a2a1ea3ca7c8392cf5395f891
BluettDream <tianmh0625@qq.com>
2024-03-05 23:02:01 +0800

    update 使用properties占位符的方式,用url成功获取resource/mapper下的文件

--- src/main/java/org/bluett/util/DBUtil.java
+++ src/main/java/org/bluett/util/DBUtil.java
@@ -6,21 +6,23 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bluett.MainApplication;
 import org.bluett.entity.SystemCache;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 public class DBUtil {
     private static final Logger log = LogManager.getLogger(DBUtil.class);
-    private static final String URL = "jdbc:sqlite:" + SystemCache.DATABASE_PATH;
+    private static final String DB_URL = "jdbc:sqlite:" + SystemCache.DATABASE_PATH;
+    private static final URL MAPPER_URL = MainApplication.class.getResource("/mapper");
     private static SqlSessionFactory FACTORY;
 
     static {
         String resource = "mybatis-config.xml";
         try (InputStream stream = Resources.getResourceAsStream(resource)) {
-            FACTORY = new SqlSessionFactoryBuilder().build(stream,
-                    fillDatabase(null, URL, null, null));
+            FACTORY = new SqlSessionFactoryBuilder().build(stream, fillProperties());
         }catch (Exception e){
             log.error("数据库初始化失败", e);
         }
@@ -32,12 +34,11 @@ public class DBUtil {
         return FACTORY.openSession();
     }
 
-    private static Properties fillDatabase(String driver, String url, String username, String password){
+    private static Properties fillProperties(){
+        if(MAPPER_URL == null) throw new RuntimeException("mapper文件加载失败");
         Properties properties = new Properties();
-        if(driver != null) properties.setProperty("driver", driver);
-        if(url != null) properties.setProperty("url", url);
-        if(username != null) properties.setProperty("username", username);
-        if(password != null) properties.setProperty("password", password);
+        properties.setProperty("dbUrl", DB_URL);
+        properties.setProperty("mapperUrl", "file://" + MAPPER_URL.getPath());
         return properties;
     }
 }
