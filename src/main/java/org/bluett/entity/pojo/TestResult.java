2db9add8bb485e934eb898fef12923c12a37a314
Company <bluetian1st@gmail.com>
2024-03-06 13:13:20 +0800

    add code

--- src/main/java/org/bluett/entity/pojo/TestResult.java
+++ src/main/java/org/bluett/entity/pojo/TestResult.java
@@ -1,19 +1,17 @@
 package org.bluett.entity.pojo;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum TestResult {
-    READY("READY"),
-    RUNNING("RUNNING"),
-    FINISH("FINISH"),
-    SUCCESS("SUCCESS"),
-    FAIL("FAIL");
+    READY("READY", 0),
+    RUNNING("RUNNING", 1),
+    FINISH("FINISH", 2),
+    SUCCESS("SUCCESS", 3),
+    FAIL("FAIL", 4);
 
     private final String name;
-
-    TestResult(String name) {
-        this.name = name;
-    }
-
+    private final int code;
 }
